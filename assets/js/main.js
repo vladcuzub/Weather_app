279f701f73a19e058a9af998805cbeafc8ec17a6
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2023-07-26 15:42:03 +0200

    some changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,5 @@
 // "use strict"
 
-// let city = document.querySelector('.city');
-// let temp = document.querySelector('.temp');
-// let humidity = document.querySelector('.humidity');
-// let wind = document.querySelector('.wind');
-// let key = "f99282ff1eccef0a2411844b758aaf4b";
-// let lat = '51.5073219';
-// let lon = '-0.1276474'
-
-// const cityInput = document.getElementById('cityInput');
-// const weatherApi = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
-// const geoApi = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}"
-
-// const getWeather = () => {
-//   const cityName = cityInput.value.trim();
-//   if (cityName === "") {
-//     alert("Please enter a valid city name.");
-//     return;
-//   }
-
-//   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error("Error App..please try again");
-//       }
-//       return response.json();
-//     })
-//     .then(data => {
-//       console.log(data)
-//       city.innerHTML = data.name;
-//       temp.innerHTML = `Temperature: ${convertKelvinToCelsius(data.main.temp)} &#8451;`;
-//       humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
-//       wind.innerHTML = `Wind: ${data.wind.speed} m/s`;
-//     })
-//     .catch(error => {
-//       console.error("Fetch error:", error);
-//     });
-// }
-
-// function convertKelvinToCelsius(kelvin) {
-//   return (kelvin - 273.15).toFixed(1);
-// }
-
-
-// script.js
-// script.js
-// script.js
 const cityInput = document.getElementById('cityInput');
 const city = document.querySelector('.city');
 const description = document.querySelector('.description');
